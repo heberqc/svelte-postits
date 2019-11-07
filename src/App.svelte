3e8dbf79c437f2279a6c0cdfdc638761ea05e79b
Heber Quequejana <heberqc@gmail.com>
2019-11-07 15:01:57 -0500

    add post-its

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Post from './Post.svelte'
+	export let title
+	let postsCount = 0
+	const addPostIt = async () => {
+		await new Post({
+			target: document.querySelector('.postItBoard'),
+			props: {
+				message: prompt('Enter a message:'),
+			},
+		})
+		postsCount += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		background-color: #42dee1;
+		/* color: #3fc5f0; */
+	}
+	.postItBoard {
+		padding: 1rem;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: start;
+	}
+</style>
+
+<h1>{title} ({postsCount})</h1>
+<button on:click={addPostIt}>Add Post</button>
+<div class="postItBoard"></div>
---
+++ src/Post.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let message
+</script>
+
+<style>
+	.postit {
+		background-color: #eef5b2;
+		box-shadow: 5px 5px 10px #282828;
+		box-shadow: 5px 5px 10px rgba(40,40,40,.8);
+		padding: 1rem;
+		margin: .5rem;
+		width: 16rem;
+	}
+	.message {
+		min-height: 3rem;
+		max-height: 3rem;
+	}
+</style>
+
+<div class="postit">
+	<span class="message">{message}</span>
+</div>
